85d2395bf7bed8281766128860c8840226c615c8
georgiadis.dimitri <georgiadis.dimitri34@gmail.com>
2024-10-10 13:59:10 +0200

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   NIVEAU 1/i-is_perfect.ts

--- NIVEAU 1/i-is_perfect.ts
+++ NIVEAU 1/i-is_perfect.ts
@@ -1,1 +1,11 @@
-function is_perfect()
+function is_perfect(n: number): boolean {
+    let res: number = 0;
+    for (let i = 1; i < n; i++) {
+        if (n % i === 0) {
+            res = i + res;
+        }
+    }
+    return res === n;
+}
+
+console.log(is_perfect(28));
